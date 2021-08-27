3b234bf0e9a83b2b28f9ec5a539935058abd9753
unknown <deliorno@gmail.com>
2021-08-27 19:29:27 +0300

    api

--- app.js
+++ app.js
@@ -33,12 +33,22 @@ const allowedCors = [
   'https://deliorno.your-movies.nomoredomains.club'
 ];
 
-app.use(
-  cors({
-    origin: allowedCors,
-    credentials: true
-  })
-);
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых 
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+}
+  next();
+});
+
+// app.use(
+//   cors({
+//     origin: allowedCors,
+//     credentials: true
+//   })
+// );
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(helmet());
